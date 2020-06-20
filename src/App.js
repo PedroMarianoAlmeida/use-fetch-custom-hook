818efcd1b74008e6530c5cfa5522850a8505ee28
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-06-19 23:16:15 -0300

    Inicial funcionality working in App.js
    The program reads an URL and parameters, fetch data and exhibit the answer on the screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [ url, setUrl ] = useState("https://api.nationalize.io?");
+  const [ parameters, setParameters ] = useState([ { "name": "Jonh" },{ "name[]": "Michael" }, {key: "123"} ])
+  const [ answerFetch, setAnswerFetch ] = useState("inicialValue");
+
+  const fullAdress = (url, searchParams) => {
+    let queryParams = "";
+
+    searchParams.map( (param, index) => {
+        const key = Object.keys(param)[0];
+        queryParams += `${key}=${param[key]}`;
+        if(index < searchParams.length - 1) queryParams += '&';            
+    });
+
+    console.log(url + queryParams);
+    return url + queryParams;
+}
+
+  const getAnswerFetch = async (currentUrl, currentParameters) => {
+    let response = await fetch( fullAdress(currentUrl, currentParameters) );
+    let result = await response.json();
+    console.log( JSON.stringify(result) );
+    setAnswerFetch( JSON.stringify(result) );
+  }
+
+  getAnswerFetch(url, parameters);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> { answerFetch } </h1>
       </header>
     </div>
   );
